3a44a085b9a2d7000c36a020b520d85c62a1d013
Alexis Creuzot <alexis@monoqle.fr>
2015-12-16 17:45:11 +0100

    began network abstraction layer

--- ColourLoveSwift/Classes/Constants.swift
+++ ColourLoveSwift/Classes/Constants.swift
@@ -7,12 +7,31 @@
 //
 
 import Foundation
+import Alamofire
 
-struct Constants {
-    struct API {
+public struct Constants {
+    public struct API{
         static let baseURL = "http://www.colourlovers.com/api"
         static let colorsURL = baseURL+"/colors"
         static let palettesURL = baseURL+"/palettes"
         static let patternsURL = baseURL+"/patterns"
     }
 }
+
+//extension URLRequestConvertible{
+//    
+//    public func request(
+//        method: Alamofire.Method,
+//        endpoint: Constants.API,
+//        parameters: [String: AnyObject]? = nil)
+//        -> Request
+//    {
+//        return Manager.sharedInstance.request(
+//            method,
+//            endpoint,
+//            parameters: parameters,
+//            encoding: .URL,
+//            headers: nil
+//        )
+//    }
+//}
